d5eb2f15857e58771138f9737b823f0830a2a224
annaign <anna.a.ignatenkova@yandex.ru>
2019-04-16 08:53:13 +0300

    fix - кавычки, запятые

--- index.php
+++ index.php
@@ -7,41 +7,41 @@ $categories = ["Доски и лыжи", "Крепления", "Ботинки",
 
 $lots = [
     [
-        "title" => "2014 Rossignol District Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "10999",
-        "image_url" => "img/lot-1.jpg"
+        'title' => "2014 Rossignol District Snowboard",
+        'category' => "Доски и лыжи",
+        'price' => "10999",
+        'image_url' => "img/lot-1.jpg"
     ],
     [
-        "title" => "DC Ply Mens 2016/2017 Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => "159999",
-        "image_url" => "img/lot-2.jpg"
+        'title' => "DC Ply Mens 2016/2017 Snowboard",
+        'category' => "Доски и лыжи",
+        'price' => "159999",
+        'image_url' => "img/lot-2.jpg"
     ],
     [
-        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "Крепления",
-        "price" => "8000",
-        "image_url" => "img/lot-3.jpg"
+        'title' => "Крепления Union Contact Pro 2015 года размер L/XL",
+        'category' => "Крепления",
+        'price' => "8000",
+        'image_url' => "img/lot-3.jpg"
     ],
     [
-        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
-        "category" => "Ботинки",
-        "price" => "10999",
-        "image_url" => "img/lot-4.jpg"
+        'title' => "Ботинки для сноуборда DC Mutiny Charocal",
+        'category' => "Ботинки",
+        'price' => "10999",
+        'image_url' => "img/lot-4.jpg"
     ],
     [
-        "title" => "Куртка для сноуборда DC Mutiny Charocal",
-        "category" => "Одежда",
-        "price" => "7500",
-        "image_url" => "img/lot-5.jpg"
+        'title' => "Куртка для сноуборда DC Mutiny Charocal",
+        'category' => "Одежда",
+        'price' => "7500",
+        'image_url' => "img/lot-5.jpg"
     ],
     [
-        "title" => "Маска Oakley Canopy",
-        "category" => "Разное",
-        "price" => "5400",
-        "image_url" => "img/lot-6.jpg"
-    ]
+        'title' => "Маска Oakley Canopy",
+        'category' => "Разное",
+        'price' => "5400",
+        'image_url' => "img/lot-6.jpg"
+    ],
 ];
 ?>
 <!DOCTYPE html>
