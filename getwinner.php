9fa6646cc33caf6b17a2d9234b47a68cfb41c591
annaign <anna.a.ignatenkova@yandex.ru>
2019-05-24 11:17:15 +0300

    fix mail

--- getwinner.php
+++ getwinner.php
@@ -10,7 +10,7 @@ $sql = "SELECT lots.id AS id_lot, lots.lot_title, bets.bet_price, users.user_nam
             lots.winner_id, bets.user_id AS user_winner
             FROM lots
             LEFT JOIN bets ON bets.lot_id = lots.id
-            LEFT JOIN users ON users.id = lots.user_id
+            LEFT JOIN users ON users.id = bets.user_id
             WHERE bets.id IN
                 (SELECT MAX(bets.id) FROM bets
                 LEFT JOIN lots ON lots.id = bets.lot_id
@@ -35,8 +35,11 @@ if ($new_winners !== false) {
     // Записать в лот победителем автора последней ставки
 
     foreach ($new_winners as $new_winner) {
-        $sql = "UPDATE lots SET winner_id = " . $new_winner['user_winner'] . " WHERE id = " . $new_winner['id_lot'];
-        $add_new_winers = db_insert_data($link, $sql, $data = []);
+        $sql = "UPDATE lots SET lots.winner_id = ? WHERE lots.id = ?";
+        $add_new_winers = db_insert_data($link, $sql, $data = [
+            $new_winner['user_winner'],
+            $new_winner['id_lot']
+        ]);
 
         // Отправить победителю на email письмо — поздравление с победой
         $recipients[$new_winner['user_email']] = $new_winner['user_name'];
